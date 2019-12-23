3fadf141dc2e86cf63987b02c1f0c491d2d9c2c8
Jan Turon <janturon@email.cz>
2019-12-23 16:32:32 +0100

    carousel improved

--- extensions/2-carousel/client/carousel.js
+++ extensions/2-carousel/client/carousel.js
@@ -1,42 +1,46 @@
-  document.addEventListener("DOMContentLoaded", function() {
-  var carouselize = function(item) {
-    const buttons = item.getElementsByTagName("button");
-    const leftButton = buttons[0];
-    const rightButton = buttons[1];
-    const carousel = item.getElementsByTagName("ul")[0];
-    const cards = carousel.querySelectorAll("li");
-    const span = item.getElementsByTagName("span")[0];
+document.addEventListener("DOMContentLoaded", function() {
+  var carouselize = function(carousel) {
 
-    var offset = 0;
-    const maxX = cards.length - 1;
+    // get elements
+    const buttons = carousel.getElementsByTagName("button");
+    const left = buttons[0];
+    const right = buttons[1];
+    const ul = carousel.getElementsByTagName("ul")[0];
+    const lis = ul.getElementsByTagName("li");
+    const span = carousel.getElementsByTagName("span")[0];
+
+    // hide empty description stripe
     if(span.innerHTML=="") span.style.display = "none";
-    
+
+    // rotate every 8 seconds if not hovering above
     var hovering = false;
-    item.addEventListener("mouseover", e=>hovering=true);
-    item.addEventListener("mouseout", e=>hovering=false);
+    carousel.addEventListener("mouseover", e=>hovering=true);
+    carousel.addEventListener("mouseout", e=>hovering=false);
     setInterval(function() { if(!hovering) rightButton.click(); }, 8000);
 
-    var showCard = function() {
-      const shift = carousel.offsetWidth * (-offset);
-      carousel.style.transform = `translateX(${shift}px)`;
-      const card = cards[offset];
-      span.style.display = card.dataset.text.length>0 ? "block" : "none";
-      span.innerHTML = card.dataset.text;
+    // shift to li at position pos and show or hide description stripe
+    var showCard = function(pos) {
+      const shift = ul.offsetWidth * (-offset);
+      ul.style.transform = `translateX(${shift}px)`;
+      const li = lis[pos];
+      span.style.display = li.dataset.text.length>0 ? "block" : "none";
+      span.innerHTML = li.dataset.text;
     }
 
-    leftButton.addEventListener("click", function() {
-      if(offset==0) offset = maxX;
+    // left and right button clicks
+    var offset = 0;
+    left.addEventListener("click", function() {
+      if(offset==0) offset = lis.length-1;
       else offset -= 1;
-      showCard();
-    })
-
-    rightButton.addEventListener("click", function() {
-      if(offset==maxX) offset = 0;
+      showCard(offset);
+    });
+    right.addEventListener("click", function() {
+      if(offset==lis.length-1) offset = 0;
       else offset += 1;
-      showCard();
-    })
+      showCard(offset);
+    });
   }
 
-  const carousels = document.querySelectorAll(".carousel");
+  const carousels = document.getElementsByClassName("carousel");
   for(var i=0; i<carousels.length; ++i) carouselize(carousels[i]);
 });
--- extensions/2-carousel/pages/example-carousel.php
+++ extensions/2-carousel/pages/example-carousel.php
@@ -7,6 +7,6 @@
   <? include "client/carousel.js"?>
 </script>
 <?=html::carousel(574, 330, array(
-  ["client/example/carousel/yes.png","index.php","I vote Yes!"],
-  ["client/example/carousel/no.png","index.php","I vote No!"]
+  ["client/example-carousel-yes.png","index.php","I vote Yes!"],
+  ["client/example-carousel-no.png","index.php","I vote No!"]
 ))?>
